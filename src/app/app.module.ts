dfcd6caceb644f346045aea630d3e1f624b1a1db
Anthony Sneed <tony@tonysneed.com>
2016-10-30 04:29:37 +0100

    Added bootstrap - See: https://github.com/valor-software/ng2-bootstrap/blob/development/docs/getting-started/ng-cli.md Added margin to app.component.css-html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AlertModule } from 'ng2-bootstrap/ng2-bootstrap';
 
 import { AppComponent } from './app.component';
 import { ProductsComponent } from './products/products.component';
@@ -14,7 +15,8 @@ import { ProductsComponent } from './products/products.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AlertModule
   ],
   providers: [],
   bootstrap: [AppComponent]
