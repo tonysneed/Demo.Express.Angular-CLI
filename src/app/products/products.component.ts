fe960363ba2865da8ef520f9c8fd6cedfb3dbb8c
Anthony Sneed <tony@tonysneed.com>
2016-10-30 04:30:12 +0100

    Added products component

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from './product';
 
 @Component({
   selector: 'app-products',
-  templateUrl: './products.component.html',
-  styleUrls: ['./products.component.css']
+  templateUrl: './products.component.html'
 })
 export class ProductsComponent implements OnInit {
+
+  title: string = 'Products';
+  products: Product[];
 
   constructor() { }
 
   ngOnInit() {
+    this.products = [
+            new Product(1, 'Product 1', 10),
+            new Product(2, 'Product 2', 20),
+        ];
   }
-
 }
